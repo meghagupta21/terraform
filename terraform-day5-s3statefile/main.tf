8a4a958e2e90fd279949f65b36f836e30709553d
meghagupta21 <guptameghamanoj@gmail.com>
2026-09-23 15:52:40 +0530

    ec2 name

--- terraform-day5-s3statefile/main.tf
+++ terraform-day5-s3statefile/main.tf
@@ -11,6 +11,10 @@ resource "aws_vpc" "vpc" {
         Name = "my-vpc2"
     }
 }
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "10.0.1.0/24"
+}
 resource "aws_instance" "myec2" {
     ami           = "ami-0b2c9d1f3edcfd709"
     instance_type = "t2.micro"
