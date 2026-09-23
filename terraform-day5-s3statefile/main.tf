a8a3344011ffc9e1d1faeca1268644f72af5ce69
meghagupta21 <guptameghamanoj@gmail.com>
2026-09-23 15:58:43 +0530

    ec2 name

--- terraform-day5-s3statefile/main.tf
+++ terraform-day5-s3statefile/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnet" {
 }
 resource "aws_instance" "myec2" {
     ami           = "ami-0b2c9d1f3edcfd709"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     subnet_id     = aws_subnet.subnet.id
     tags = {
         Name = "my-ec2-instance"
